aa05419b18437b18ea2dcb43e9288646e208493c
Atanazio01 <marcosd.atanazio@gmail.com>
2022-04-03 18:57:40 -0300

    ImplicitConversionAndCasting

---
+++ 03-session/ImplicitConversionAndCasting/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ImplicitConversionAndCasting {
+  class Program {
+    static void Main(string[] args) {
+      float x = 4.5f;
+      // conversao implícita
+      double y = x;
+
+      double a;
+      float b;
+      a = 5.1;
+      // conversão explícita
+      b = (float)a; // casting
+
+      int c = (int)a; // casting
+
+      System.Console.WriteLine(y);
+      System.Console.WriteLine(b);
+      System.Console.WriteLine(c);
+    }
+  }
+}
