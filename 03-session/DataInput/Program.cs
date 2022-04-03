92468ddc3662558e2f1c1329899da45ac095c48c
Atanazio01 <marcosd.atanazio@gmail.com>
2022-04-03 20:48:16 -0300

    DataInput part2

--- 03-session/DataInput/Program.cs
+++ 03-session/DataInput/Program.cs
@@ -1,30 +1,29 @@
-﻿using System;
-
+﻿using System.Globalization;
 namespace DataInput
 {
-    class Program
+  class Program
     {
         static void Main(string[] args)
         {
-            string frase = Console.ReadLine();
-            string x = Console.ReadLine();
-            string y = Console.ReadLine();
-            string z = Console.ReadLine();
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             string[] vet = Console.ReadLine().Split(' ');
 
-            string a = vet[0];
-            string b = vet[1];
-            string c = vet[2];
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
 
             Console.WriteLine("Você digitou:");
-            Console.WriteLine(frase);
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(z);
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(c);
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
