209e60f586a78c5c700a8f641a41c16be30138f3
Atanazio01 <marcosd.atanazio@gmail.com>
2022-04-05 20:33:59 -0300

    Proposed Exercises Part 1 Exercise 3

---
+++ 03-session/ProposedExercisesPart1/Exercise03/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercise03 {
+  class Program {
+    static void Main(string[] args){
+      int a = int.Parse(Console.ReadLine());
+      int b = int.Parse(Console.ReadLine());
+      int c = int.Parse(Console.ReadLine());
+      int d = int.Parse(Console.ReadLine());
+
+      int diferenca = (a*b-c*d);
+
+      Console.WriteLine($"DIFERENÇA = {diferenca}");
+    }
+  }
+}
