59700a8c9b0f2695af708835c4e566ff278347e0
Atanazio01 <marcosd.atanazio@gmail.com>
2022-04-05 20:45:28 -0300

    Proposed Exercises Part 1 Exercise 4

---
+++ 03-session/ProposedExercisesPart1/Exercise04/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercise04 {
+  class Program {
+    static void Main(string[] args) {
+      int employeeNumber = int.Parse(Console.ReadLine());
+      int workedHours = int.Parse(Console.ReadLine());
+      double hourlySalary = double.Parse(Console.ReadLine());
+
+      double salary = workedHours * hourlySalary;
+
+      Console.WriteLine($"NUMBER = {employeeNumber}");
+      Console.WriteLine($"SALARY = U$ {salary.ToString("F2")}");
+    }
+  }
+}
