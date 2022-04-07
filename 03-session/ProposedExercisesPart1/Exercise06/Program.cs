4fc2759b8491e969ae3d5da0f6661f44c31c1259
Atanazio01 <marcosd.atanazio@gmail.com>
2022-04-06 21:02:11 -0300

    Proposed Exercises Part 1 Exercise 6

---
+++ 03-session/ProposedExercisesPart1/Exercise06/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercise06 {
+  class Program {
+    static void Main(string[] args) {
+      string[] values = Console.ReadLine().Split(' ');
+      double a = double.Parse(values[0]);
+      double b = double.Parse(values[1]);
+      double c = double.Parse(values[2]);
+      double pi = 3.14159;
+
+      double triangulo = (a * c) / 2;
+      double circulo = pi * Math.Pow(c, 2);
+      double trapezio = ((a+b)*c) / 2;
+      double quadrado = b*b;
+      double retangulo = a*b;
+
+
+      Console.WriteLine($"TRIANGULO: {triangulo.ToString("F3")}");
+      Console.WriteLine($"CIRCULO: {circulo.ToString("F3")}");
+      Console.WriteLine($"TRAPEZIO: {trapezio.ToString("F3")}");
+      Console.WriteLine($"QUADRADO: {quadrado.ToString("F3")}");
+      Console.WriteLine($"RETANGULO: {retangulo.ToString("F3")}");
+    }
+  }
+}
