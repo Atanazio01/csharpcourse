2e21e49523967651b15e59c6497166435278f5a5
Atanazio01 <marcosd.atanazio@gmail.com>
2022-04-15 14:20:45 -0300

    Conditional structure if-else

---
+++ 03-session/ConditionalStructure/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConditionalStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            Console.WriteLine("Bom dia");
+
+            if (x < 5)
+            {
+                Console.WriteLine("Boa tarde");
+            }
+            Console.WriteLine("Boa noite");
+        }
+    }
+}
