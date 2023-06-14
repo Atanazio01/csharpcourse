2d0324e6401914dbfe53aec81abe8314cd739505
Atanazio01 <marcosd.atanazio@gmail.com>
2023-06-14 11:03:39 -0300

    Basic types in CSharp part 1

---
+++ section-3/BasicTypesInCSharp/BasicTypesInCSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte x = 100; // 0 to 255
+            byte n1 = 126; // -126 to 127
+            int n2 = 1000; // -2147483647 to 2147483647
+            int n3 = 2147483647; // -2147483647 to 2147483647
+            long n4 = 2147483648L; // -9.223.372.036.854.775.999 to -9.223.372.036.854.775.999
+
+
+            Console.WriteLine(x);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+        }
+    }
+}
