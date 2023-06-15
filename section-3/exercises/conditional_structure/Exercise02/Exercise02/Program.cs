63c0c279ced82df85eeab625b03f0d4c28ce5162
Atanazio01 <marcosd.atanazio@gmail.com>
2023-06-15 15:59:51 -0300

    Conditional structure exercise 02

---
+++ section-3/exercises/conditional_structure/Exercise02/Exercise02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise01
+
+/*
+ * Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
+*/
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+        }
+    }
+}
