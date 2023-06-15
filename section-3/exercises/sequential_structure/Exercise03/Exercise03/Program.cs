32ebf20b7289cfa33a2ed683b7fb19fb7cba9a5a
Atanazio01 <marcosd.atanazio@gmail.com>
2023-06-15 14:55:11 -0300

    Sequential structure exercise 03

---
+++ section-3/exercises/sequential_structure/Exercise03/Exercise03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise01
+
+/*
+ * Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto 
+de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D)
+*/
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            int difference = (a * b) - (c * d);
+            Console.WriteLine($"DIFERENÇA = {difference}");
+        }
+    }
+}
