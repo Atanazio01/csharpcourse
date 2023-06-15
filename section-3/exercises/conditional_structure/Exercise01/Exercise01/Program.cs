7e7ff226c1d0c27c5a10df7c284d2265507a697b
Atanazio01 <marcosd.atanazio@gmail.com>
2023-06-15 15:57:55 -0300

    Conditional structure exercise 01

---
+++ section-3/exercises/conditional_structure/Exercise01/Exercise01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise01
+
+/*
+ * Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.
+*/
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (number < 0)
+            {
+                Console.WriteLine("NEGATIVO");
+            }
+            else
+            {
+                Console.WriteLine("POSITIVO");
+            }
+        }
+    }
+}
