545c00117c283bb20e870d2f7f2970b668b12b69
Atanazio01 <marcosd.atanazio@gmail.com>
2023-06-15 16:06:01 -0300

    Conditional structure exercise 03

---
+++ section-3/exercises/conditional_structure/Exercise03/Exercise03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise01
+
+/*
+ * Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao 
+Multiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem poder ser digitados em 
+ordem crescente ou decrescente.
+*/
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] number = Console.ReadLine().Split(' ');
+            int number1 = int.Parse(number[0]);
+            int number2 = int.Parse(number[1]);
+
+            if (number1 % number2 == 0 || number2 % number1 == 0)
+            {
+                Console.WriteLine("São multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Não são multiplos");
+            }
+        }
+    }
+}
