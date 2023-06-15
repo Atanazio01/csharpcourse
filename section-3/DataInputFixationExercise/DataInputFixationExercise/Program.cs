170adfe6bcf8702de04e3434ed16d63f2b189f29
Atanazio01 <marcosd.atanazio@gmail.com>
2023-06-15 14:20:59 -0300

    Fixation Exercise for data input in CSharp

---
+++ section-3/DataInputFixationExercise/DataInputFixationExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace DataInputFixationExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int bedrooms = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o preço de um produto:");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com seu último nome, idade e altura (mesma linha):");
+            string[] userData = Console.ReadLine().Split(' ');
+
+            string lastName = userData[0];
+            int age = int.Parse(userData[1]);
+            double height = double.Parse(userData[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(name);
+            Console.WriteLine(bedrooms);
+            Console.WriteLine(price.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(lastName);
+            Console.WriteLine(age);
+            Console.WriteLine(height.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
