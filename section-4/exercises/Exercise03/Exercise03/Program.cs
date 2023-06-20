aab488cd60594217c1d03083b05d9c7705b3564f
Atanazio01 <marcosd.atanazio@gmail.com>
2023-06-20 16:59:27 -0300

    Section 4 exercise 03

---
+++ section-4/exercises/Exercise03/Exercise03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            Console.Write("Nome do aluno: ");
+            student.Name = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+            student.N1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            student.N2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            student.N3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine(student);
+        }
+    }
+}
---
+++ section-4/exercises/Exercise03/Exercise03/Student.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace Exercise03
+{
+    internal class Student
+    {
+        public string Name;
+        public double N1;
+        public double N2;
+        public double N3;
+
+        public double SumOfGrades()
+        {
+            return N1 + N2 + N3;
+        }
+
+        public string CheckIsApproved()
+        {
+            if(SumOfGrades() >= 60.00)
+            {
+                return $"APROVADO";
+            }
+            else
+            {
+                double difference = 60.00 - SumOfGrades();
+                return $"REPROVADO\nFALTARAM {difference.ToString("F2", CultureInfo.InvariantCulture)} PONTOS";
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"NOTA FINAL = {SumOfGrades().ToString("F2", CultureInfo.InvariantCulture)}" +
+                $"\n{CheckIsApproved()}";
+        }
+    }
+}
