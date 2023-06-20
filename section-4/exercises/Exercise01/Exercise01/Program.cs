56f4504759bf099702fd0da2c5ca02695259f373
Atanazio01 <marcosd.atanazio@gmail.com>
2023-06-20 15:24:36 -0300

    Section 04 exercise 01

---
+++ section-4/exercises/Exercise01/Exercise01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            Console.WriteLine("Entre com a largura e altura do retângulo:");
+            rectangle.Width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            rectangle.Height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine(rectangle);
+
+        }
+    }
+}
---
+++ section-4/exercises/Exercise01/Exercise01/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace Exercise01
+{
+    internal class Rectangle
+    {
+        public double Width;
+        public double Height;
+
+        public double Area()
+        {
+            return (Width * Height);
+        }
+
+        public double Perimeter()
+        {
+            return 2 * (Width + Height);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt((Math.Pow(Width, 2) + Math.Pow(Height, 2)));
+        }
+
+        public override string ToString()
+        {
+            return $"ÁREA = {Area().ToString("F2", CultureInfo.InvariantCulture)}" +
+                $"\nPERÍMETRO = {Perimeter().ToString("F2", CultureInfo.InvariantCulture)}" +
+                $"\nDIAGONAL = {Diagonal().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
