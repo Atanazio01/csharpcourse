e5d60221fe0282922ce864e89636bbcbb1671bbc
Atanazio01 <marcosd.atanazio@gmail.com>
2023-06-20 14:01:33 -0300

    Object e ToString

---
+++ section-4/ProductClassFixationExercise/ProductClassFixationExercise/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace ProductClassFixationExercise
+{
+    internal class Product
+    {
+        public string Name;
+        public double Price;
+        public int Amount;
+
+        public double TotalAmountInStock()
+        {
+            return Price * Amount;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, " +
+                $"$ {Price.ToString("F2", CultureInfo.InvariantCulture)}, " +
+                $"{Amount} unidades, " +
+                $"Total: {TotalAmountInStock().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ section-4/ProductClassFixationExercise/ProductClassFixationExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProductClassFixationExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product();
+
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            p.Name = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade: ");
+            p.Amount = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto: " + p);
+                
+        }
+    }
+}
