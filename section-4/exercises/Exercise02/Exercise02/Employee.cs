d6f4b27053d3478a9245a453d6fce4d413fa1d8b
Atanazio01 <marcosd.atanazio@gmail.com>
2023-06-20 15:44:55 -0300

    Section 04 exercise 02

---
+++ section-4/exercises/Exercise02/Exercise02/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace Exercise02
+{
+    internal class Employee
+    {
+        public string Name;
+        public double GrossSalary;
+        public double Tax;
+
+        public double NetSalary()
+        {
+            return GrossSalary - Tax;
+        }
+
+        public void IncreaseSalary(double percentage)
+        {
+            GrossSalary += percentage * (GrossSalary / 100.0);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, $ {NetSalary().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ section-4/exercises/Exercise02/Exercise02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            Console.Write("Nome: ");
+            employee.Name = Console.ReadLine();
+
+            Console.Write("Salário bruto: ");
+            employee.GrossSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Imposto: ");
+            employee.Tax = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Funcionário: " + employee);
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            employee.IncreaseSalary(percentage);
+
+            Console.WriteLine("Dados atualizados: " + employee);
+        }
+    }
+}
