4accde87d1c98819ac36ceaf107ef5efedd4640a
Atanazio01 <marcosd.atanazio@gmail.com>
2023-06-21 11:42:53 -0300

    Fixation exercise - Currency converter

---
+++ section-4/CurrencyConverterFixationExercise/CurrencyConverterFixationExercise/CurrencyConverter.cs
@@ -0,0 +1,12 @@
+﻿namespace CurrencyConverterFixationExercise
+{
+    internal class CurrencyConverter
+    {
+        public static double Iof = 6.38;
+        public static double Converter(double quotation, double dollars)
+        {
+            double total = quotation * dollars;
+            return total + (Iof * (total / 100));
+        }
+    }
+}
---
+++ section-4/CurrencyConverterFixationExercise/CurrencyConverterFixationExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace CurrencyConverterFixationExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double quotation = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            double dollars = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double total = CurrencyConverter.Converter(quotation, dollars);
+            Console.WriteLine("Valor a ser pago em reais = " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
