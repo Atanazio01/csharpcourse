a46d8aa80e75e69d0f8876f841f18779bb1357e1
Atanazio01 <marcosd.atanazio@gmail.com>
2023-06-22 15:39:07 -0300

    Exerxice of fixation about vectors with a product class

---
+++ section-6/VectorsFixationExercise/VectorsFixationExercise/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace VectorsFixationExercise
+{
+    internal class Product
+    {
+        public string Name { get; private set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+    }
+}
---
+++ section-6/VectorsFixationExercise/VectorsFixationExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace VectorsFixationExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Product[] products = new Product[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string name  = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                products[i] = new Product(name, price); // poderia ser usada a sintaxe automatica sem construct {}
+            }
+
+            double sum = 0.0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += products[i].Price;
+            }
+
+            double avg = sum / n;
+
+            Console.WriteLine("AVERAGE PRICE = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
