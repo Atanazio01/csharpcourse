d23ca221c35f2cf6b5d764d2e9d26819007cde3d
Atanazio01 <marcosd.atanazio@gmail.com>
2023-06-22 11:47:14 -0300

    Exercise of fixation constructors, word this, overloading and encapsulation

---
+++ section-5/BankAccountFixationExercise/BankAccountFixationExercise/BankAccount.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+
+namespace BankAccountFixationExercise
+{
+    internal class BankAccount
+    {
+        private string _name;
+        public int Number { get; set; }
+        public double Balance { get; private set; }
+
+        public BankAccount(int number, string name)
+        {
+            Number = number;
+            Name = name;
+        }
+
+        public BankAccount(int number, string name, double initialDeposit) : this(number, name)
+        {
+            Deposit(initialDeposit);
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        public void Deposit(double quantity)
+        {
+            Balance += quantity;
+        }
+
+        public void WithDraw(double quantity)
+        {
+            Balance -= (quantity + 5.00);
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {Number}, Titular: {_name}, Saldo: $ {Balance.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ section-5/BankAccountFixationExercise/BankAccountFixationExercise/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace BankAccountFixationExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account;
+
+            Console.Write("Entre com o número da conta: ");
+            int number = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Entre com o nome do títular da conta: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char haveInitialDeposit = char.Parse(Console.ReadLine());
+            if (haveInitialDeposit == 's' || haveInitialDeposit == 'S')
+            {
+                Console.Write("Entre com o valor do depósito inicial: ");
+                double initialDeposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account = new BankAccount(number, name, initialDeposit);
+            }else
+            {
+                account = new BankAccount(number, name);
+            }
+
+            Console.WriteLine("\nDados da conta:");
+            Console.WriteLine(account);
+
+            Console.Write("\nEntre com um valor para depósito: ");
+            double deposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            account.Deposit(deposit);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(account);
+
+            Console.Write("\nEntre com um valor para saque: ");
+            double withdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            account.WithDraw(withdraw);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(account);
+        }
+    }
+}
