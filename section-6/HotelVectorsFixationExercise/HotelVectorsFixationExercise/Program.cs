06b43105de823d92f34e42ca2e4aee950470b9af
Atanazio01 <marcosd.atanazio@gmail.com>
2023-07-05 14:14:39 -0300

    Exerxice of fixation about vectors with a students class

---
+++ section-6/HotelVectorsFixationExercise/HotelVectorsFixationExercise/Program.cs
@@ -0,0 +1,37 @@
+﻿using HotelVectorsFixationExercise;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Students[] students = new Students[10];
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Aluguel #{i}:");
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int room = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                students[room] = new Students(name, email);
+            }
+
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 1;i <= students.Length;i++) {
+                if (students[i] != null)
+                {
+                    Console.WriteLine($"{i}: {students[i]}");
+                }
+            }
+        }
+    }
+}
---
+++ section-6/HotelVectorsFixationExercise/HotelVectorsFixationExercise/Students.cs
@@ -0,0 +1,19 @@
+﻿namespace HotelVectorsFixationExercise
+{
+    internal class Students
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Students(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Email}";
+        }
+    }
+}
