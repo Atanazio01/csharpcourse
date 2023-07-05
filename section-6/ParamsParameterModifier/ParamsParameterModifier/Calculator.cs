c8075e0616a106917fbe1a1cf3f9aeb58b8dc8e0
Atanazio01 <marcosd.atanazio@gmail.com>
2023-07-05 11:32:06 -0300

    Params parameter modifier

---
+++ section-6/ParamsParameterModifier/ParamsParameterModifier/Calculator.cs
@@ -0,0 +1,17 @@
+﻿namespace ParamsParameterModifier
+{
+    class Calculator
+    {
+        public static int Sum(params int[] numbers) // o modificador params informa ao compilador que irá 
+                                                    // receber uma funcao que recebe um número variavel de valores
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+    }
+}
+
---
+++ section-6/ParamsParameterModifier/ParamsParameterModifier/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ParamsParameterModifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int s1 = Calculator.Sum(new int[] { 2, 3, 4, 5, 6, 7 }); // sem o modificador params 
+            int s2 = Calculator.Sum(1, 2);
+
+            //Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+    }
+}
