9a26264e9aa9a18c57f6b92da2e9a6a941f3c805
Atanazio01 <marcosd.atanazio@gmail.com>
2023-07-05 15:20:27 -0300

    Loop foreach

---
+++ section-6/LoopForEach/LoopForEach/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vect = new string[] { "Maria", "Alex", "Bob" };
+
+            foreach (string s in vect)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
