57370900de3303ac9da841238de4015c54ce0f78
Atanazio01 <marcosd.atanazio@gmail.com>
2023-07-06 13:49:23 -0300

    Matrices

---
+++ section-6/Matrices/Matrices/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] mat = new double[4, 3];
+
+            Console.WriteLine(mat.Length); // Quantos elementos a matriz possui
+
+            Console.WriteLine(mat.Rank); // Quantas dimensões a matriz tem
+            
+            Console.WriteLine(mat.GetLength(0)); // Quantos elementos na primeira dimensão(linhas)
+
+            Console.WriteLine(mat.GetLength(1)); // Quantos elementos na segunda dimensao(colunas)
+        }
+    }
+}
