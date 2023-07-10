f6fe9b01ac388d7d141e4b7f427ef891a75babca
Atanazio01 <marcosd.atanazio@gmail.com>
2023-07-10 16:35:05 -0300

    Switch Case in CSharp

---
+++ section-7/SwitchCaseInCSharp/SwitchCaseInCSharp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SwitchCaseInCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            string day = "";
+
+            while (day != "Invalid Date")
+            {
+                Console.Write("Informe o dia da semana para ver sua tradução em inglês: ");
+                x = Console.ReadLine();
+                switch (x)
+                {
+                    case "domingo":
+                        day = "Sunday"; break;
+                    case "segunda":
+                        day = "Monday"; break;
+                    case "terça":
+                        day = "Tuesday"; break;
+                    case "quarta":
+                        day = "Wednesday"; break;
+                    case "quinta":
+                        day = "Thursday"; break;
+                    case "sexta":
+                        day = "Friday"; break;
+                    case "sábado":
+                        day = "Saturday"; break;
+                    default:
+                        day = "Invalid Date"; break;
+                }
+
+                Console.WriteLine($"Day: {day}");
+            }
+        }
+    }
+}
