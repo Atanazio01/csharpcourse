23bd96308a1deea542d8188242da8f3e77b202ad
Atanazio01 <marcosd.atanazio@gmail.com>
2023-07-11 16:15:56 -0300

    Csharp Enumerations

---
+++ section-9/Enum1InCsharp/Enum1InCsharp/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using Enum1InCsharp.Entities.Enums;
+
+namespace Enum1InCsharp.Entities
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Moment}, {Status}";
+        }
+    }
+}
---
+++ section-9/Enum1InCsharp/Enum1InCsharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using Enum1InCsharp.Entities;
+using Enum1InCsharp.Entities.Enums;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+        }
+    }
+}
