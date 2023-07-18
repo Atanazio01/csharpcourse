ac5f742fa2308938a1b7609e4c4b5c1481a97956
Atanazio01 <marcosd.atanazio@gmail.com>
2023-07-18 13:15:20 -0300

    Resolved Exercise - Composition

---
+++ section-9/Composition1-Csharp/Composition1-Csharp/Entities/Department.cs
@@ -0,0 +1,14 @@
+﻿namespace Composition1_Csharp.Entities
+{
+    internal class Department
+    {
+        public string Name { get; set; }
+
+        public Department() { }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
