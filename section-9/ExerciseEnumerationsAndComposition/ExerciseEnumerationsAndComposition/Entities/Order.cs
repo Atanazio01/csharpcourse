7257da9fb22cf6d4d231e7ff23433ced9446b6db
Atanazio01 <marcosd.atanazio@gmail.com>
2023-07-18 15:09:36 -0300

    Exercise posts

---
+++ section-9/ExerciseEnumerationsAndComposition/ExerciseEnumerationsAndComposition/Entities/Order.cs
@@ -0,0 +1,38 @@
+﻿using ExerciseEnumerationsAndComposition.Entities.Enums;
+
+namespace ExerciseEnumerationsAndComposition.Entities
+{
+    internal class Order
+    {
+        public DateTime Moment { get; set; } = DateTime.Now;
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Item { get; set; } = new List<OrderItem>();
+
+        public Order() { }
+
+        public Order(OrderStatus status)
+        {
+            Status = status;
+        }
+
+        public void addItem(OrderItem item)
+        {
+            Item.Add(item);
+        }
+
+        public void removeItem(OrderItem item)
+        {
+           Item.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0;
+            foreach (OrderItem item in Item)
+            {
+                sum += item.SubTotal();
+            }
+            return sum;
+        }
+    }
+}
