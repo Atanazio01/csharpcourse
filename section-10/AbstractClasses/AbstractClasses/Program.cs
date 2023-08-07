0246c841286eb0afe1fc95ec194ed5e6707f4a41
Atanazio01 <marcosd.atanazio@gmail.com>
2023-08-07 16:00:52 -0300

    Abstract classes

---
+++ section-10/AbstractClasses/AbstractClasses/Program.cs
@@ -0,0 +1,38 @@
+﻿using AbstractClasses.Entities;
+using System.Globalization;
+
+namespace AbstractClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Account> accounts = new List<Account>();
+
+            accounts.Add(new SavingsAccount(1001, "Alex", 500.0, 0.01));
+            accounts.Add(new BusinessAccount(1002, "Maria", 500.0, 400.0));
+            accounts.Add(new SavingsAccount(1003, "Bob", 500.0, 0.01));
+            accounts.Add(new BusinessAccount(1004, "Anna", 500.0, 500.0));
+
+            double sum = 0.0;
+
+            foreach (var account in accounts)
+            {
+                sum += account.Balance;
+            }
+
+            Console.WriteLine($"Total Balance: {sum.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            foreach(var account in accounts)
+            {
+                account.Withdraw(10.0);
+            }
+
+            foreach(var account in accounts)
+            {
+                Console.WriteLine($"Updated balance for account " +
+                    $"{account.Number}: {account.Balance.ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+        }
+    }
+}
