118072dbac86674b339736c1ec48cd08dc5b1843
Atanazio01 <marcosd.atanazio@gmail.com>
2023-08-10 10:47:16 -0300

    Directory and DirectoryInfo

---
+++ section-13/DirectoryAndDirectoryInfo/DirectoryAndDirectoryInfo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DirectoryAndDirectoryInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\myfolder";
+
+            try
+            {
+                IEnumerable<string> folder = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS:");
+                foreach (string item in folder)
+                {
+                    Console.WriteLine(item);
+                }
+
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES:");
+                foreach (string item in files)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Directory.CreateDirectory(path + @"\newfolder");
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
