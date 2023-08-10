add2b310979d827194104509ef666da18cbbe27c
Atanazio01 <marcosd.atanazio@gmail.com>
2023-08-10 15:07:22 -0300

    Interfaces Solucion part 1

---
+++ section-14/InterfaceSolution/InterfaceSolution/Program.cs
@@ -0,0 +1,22 @@
+﻿using InterfaceSolution.Entities;
+using System;
+using System.Globalization;
+
+namespace InterfaceSolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+        }
+    }
+}
