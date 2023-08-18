7da9e2455dffebfe7c052217919ce639109ccbf3
Atanazio01 <marcosd.atanazio@gmail.com>
2023-08-18 10:06:15 -0300

    Generics part1

---
+++ section-15/Generics/Generics/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService printService = new PrintService();
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                printService.AddValue(int.Parse(Console.ReadLine()));
+            }
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+        }
+    }
+}
